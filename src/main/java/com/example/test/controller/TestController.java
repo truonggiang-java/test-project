b04443eb55bf04d1e6282eb47060ba48d1f95997
Giang Cao Trường <giangct@vega.com.vn>
2023-03-27 14:34:20 +0700

    code

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+	
+	@GetMapping
+	public String hello() {
+		return "hello word";
+	}
+}
